eb8fc27dddb9aa6215996b0dd57cc858be03a92c
Fedor090607 <123183014+Fedor090607@users.noreply.github.com>
2025-12-21 16:47:09 +0300

    Update 4zd.py

--- 1. Задания/Парсер автосервисов/4zd.py
+++ 1. Задания/Парсер автосервисов/4zd.py
@@ -10,7 +10,7 @@ html_content = response.read().decode()
 with open('html.txt', mode='w', encoding='utf8') as file:
     file.write(html_content)
 
-#\s* для удаления лишних пробелов
+#\s* для удал ненужных пробелов
 re_algorithm = r'class="org-widget-header__title-link">\s*([^<]+?)\s*</a>.*?' \
                r'org-widget-header__meta--location">\s*([^<]+?)\s*</span>.*?' \
                r'<dt class="spec__index"><span class="spec__index-inner">Телефон</span></dt>.*?' \
@@ -18,14 +18,14 @@ re_algorithm = r'class="org-widget-header__title-link">\s*([^<]+?)\s*</a>.*?' \
                r'<dt class="spec__index"><span class="spec__index-inner">Часы работы</span></dt>.*?' \
                r'<dd class="spec__value">\s*([^<]+?)\s*</dd>'
 
-# Поиск совпадений по регулярному выражению
+# поиск совпадений по регуляркам
 matches = re.findall(re_algorithm, html_content, re.DOTALL)
 
 cleaned_matches = [(name.strip(), address.strip(), phone.strip(), hours.strip()) for name, address, phone, hours in matches]
 
-# Запись результатов в CSV-файл с разделителем точка с запятой
+# запись результатов в csv с запятой
 with open('output.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
-    writer = csv.writer(csvfile, delimiter=';')  # Указываем разделитель ";"
+    writer = csv.writer(csvfile, delimiter=',')  # Указываем разделитель ";"
 
     writer.writerow(['Наименование', 'Адрес', 'Телефон', 'Часы работы'])
     #строки данных
